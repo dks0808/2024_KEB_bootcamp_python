89bc8edee32c51f93e1aaf2301c698136f70de24
light <agy91521947@gmail.com>
2024-01-16 14:42:57 +0900

    v2.1

--- day03.py
+++ day03.py
@@ -1,13 +1,16 @@
 #prime number
 number = int(input("Enter the number : "))
-count = 0
-i = 2
-while i < number:
-    if number % i == 0:
-        count = count + 1
-        break
-    i = i + 1
-if count == 0:
-    print(f'{number} is prime number')
-else:
+is_prime = True
+if number < 2:
     print(f'{number} is not prime number')
+else:
+    i = 2
+    while i < number:
+        if number % i == 0:
+            is_prime =False
+            break
+        i = i + 1
+    if is_prime: #is prime 자체가 불린 형임으로 ==를 붙여줄 필요가 없다.
+        print(f'{number} is prime number')
+    else:
+        print(f'{number} is not prime number')
