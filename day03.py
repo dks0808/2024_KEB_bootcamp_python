2a44873cbd7f8e70773911055dde30fd869176f3
light <agy91521947@gmail.com>
2024-01-16 15:43:40 +0900

    v2.3 btween n1, n2

--- day03.py
+++ day03.py
@@ -1,11 +1,19 @@
-univ ='inha'
-i = 0
-while i < len(univ):
-    print(univ[i], end = ' ')
-    i +=1
+#입력값은 100과 50
 
-for letter in univ:
-    print(letter, end = ' ')
+numbers = input("Enter the number1, number2.").split()
+n1 =int(numbers[0])
+n2 =int(numbers[1])
+if n1 > n2:
+    n1, n2 = n2, n1
 
-for k in range(0,len(univ), 1):
-    print(univ[k], end =' ')
+for number in range(n1,n2+1):
+    is_prime = True # is_prime 을 반복 마다 True 로 주어야함.
+
+    if number < 2:
+        pass
+    else:
+        for i in range(2, number):
+            if number % i == 0:
+                is_prime =False
+                break
+        if is_prime: print(number, end=' ')
