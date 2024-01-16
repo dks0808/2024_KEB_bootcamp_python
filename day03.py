4fc93b9e481ab9f756149cfa771af061fd0ecbad
light <agy91521947@gmail.com>
2024-01-16 14:59:34 +0900

    v2.2 for

--- day03.py
+++ day03.py
@@ -1,16 +1,11 @@
-#prime number
-number = int(input("Enter the number : "))
-is_prime = True
-if number < 2:
-    print(f'{number} is not prime number')
-else:
-    i = 2
-    while i < number:
-        if number % i == 0:
-            is_prime =False
-            break
-        i = i + 1
-    if is_prime: #is prime 자체가 불린 형임으로 ==를 붙여줄 필요가 없다.
-        print(f'{number} is prime number')
-    else:
-        print(f'{number} is not prime number')
+univ ='inha'
+i = 0
+while i < len(univ):
+    print(univ[i], end = ' ')
+    i +=1
+
+for letter in univ:
+    print(letter, end = ' ')
+
+for k in range(0,len(univ), 1):
+    print(univ[k], end =' ')
