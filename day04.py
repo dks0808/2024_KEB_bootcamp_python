8fa3a859eb24576c13b233d8565456565956329b
light <agy91521947@gmail.com>
2024-01-17 13:34:25 +0900

    3.1

--- day04.py
+++ day04.py
@@ -1,16 +1,10 @@
-subjects = ["c++", "5", "Java", "python", "Java", "9"]
+import copy
 
-# subjects.remove('Java')# 순서대로 지움
-# print(subjects)
-# del subjects[-1] #특정 값을 지움
-# print(subjects)
-#
-# subjects.pop() # 뒤엣부터 지움
-# subjects.pop(1) #앞에서부터 2번째를 지움
-#
-#
-
-subjects.sort(reverse= True) #원본이 바뀌게 됨.
-print(subjects)
-copy_subjects = sorted(subjects)
-print(copy_subjects)
+subjects = ["a",["b","c"],"d"]
+a = subjects
+b = subjects.copy()
+c = list(subjects)
+d = subjects[:]
+e = copy.deepcopy(a)
+subjects[1][1] = 'x'
+print(subjects,a,b,c,d,e)
