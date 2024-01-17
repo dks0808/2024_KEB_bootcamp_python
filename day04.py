58957ef3408ad1d031d4c1fc4e315ef06714dd49
light <agy91521947@gmail.com>
2024-01-17 10:44:43 +0900

    v2.9

--- day04.py
+++ day04.py
@@ -1,22 +1,34 @@
-numbers = input("Enter the number1, number2.").split()
-n1 =int(numbers[0])
-n2 =int(numbers[1])
-if n1 > n2:
-    n1, n2 = n2, n1
+t1 = (5)
+t2 = (5,)
+t3 = (5,7)
+t4= 5,7
+print(type(t1), type(t2), type(t3), type(t4),)
+t6 = "python", "kim"#packing
+print(type(t6), t6[1])
+subject, prof =t6 # unpacking
+print(prof)
+print(subject)
+# unpacking시 중요한 것은 짝수가 맞는 것이 중요함
 
-for number in range(n1, n2+1):
-    is_prime = True # is_prime 을 반복 마다 True 로 주어야함.
+# a, b, c = t6 #ValueError: not enough values to unpack (expected 3, got 2)
 
-    if number < 2:
-        pass
-    else:
-        i = 2
-        while i*i <= number: # i^2을 해줌으로서 소수에 대한 성능 이슈 해결
-            if number % i ==0:
-                is_prime = False
-                break
-            i += 1
-            print(i,end=' ')
+t7 = ()
+print(type(t7)) # t7도 튜플임 T/F 할 때 빈 괄호와 문자열은 F
+t8() = tuple()
+print(type('im',)) #
+print(type(('im',))) #
 
-        if is_prime: print(number, end=' ')
-# number 같은 것은 for 문 안에서만 작동하기 때문에 선언을 따로 하지 않아도 됨.
+a = ('yada',) * 3 # 문자열과 같이 3번 반복함
+print(a)
+
+t9 = 1,2,3
+t10 = 1,2
+print(t9<=t10)
+print(t9==t10)
+print(t9>=t10)
+
+# immutable이지만
+t11 =4.43,
+t12 =3.97, 4.1, 3.27
+print(t11 +t12) #임시로 출력하는 것
+t11 = t11+t12 # t11+=t12 t11과 t12의 값을 더한 값을 t11에 새로운 값(주소)을 할당한 것.즉 t11이 할당 받은 id가 달라진다.
