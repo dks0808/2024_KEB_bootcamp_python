c3a5284a65fc994c06645236a76a3ae6e38db591
light <agy91521947@gmail.com>
2024-01-17 14:43:40 +0900

    v3.3

--- day04.py
+++ day04.py
@@ -1,10 +1,20 @@
-import copy
+sugang = dict(python='kim', cpp='kang', db='kang')
+# print(sugang)
+# sugang['datastructure']= 'kim'
+# print(sugang)
+# sugang['datastructure']='park'
+# print(sugang['db'])
+# print(sugang.get('python'))
+# print(sugang.get('opensource', 'not exist'))
+for subject, professor in sugang.items():
+    print(f"{subject}'s professor is {professor}")
 
-subjects = ["a",["b","c"],"d"]
-a = subjects
-b = subjects.copy()
-c = list(subjects)
-d = subjects[:]
-e = copy.deepcopy(a)
-subjects[1][1] = 'x'
-print(subjects,a,b,c,d,e)
+for k in sugang.keys(): #keys를 안써줘도 키가자동으로나옴 키는 디폴트값임
+    print(k)
+for k in sugang:  # keys를 안써줘도 키가자동으로나옴 키는 디폴트값임
+    print(k)
+for v in sugang.values():
+    print(v)
+
+for v in sugang.items():# tuple 형태로 출력 packing
+    print(v)
