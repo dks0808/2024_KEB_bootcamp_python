66320c75ad7fc5f9eb5d10dc445ef20da3cf56f1
light <agy91521947@gmail.com>
2024-01-17 09:47:20 +0900

    v2.7

--- day03Homework_1.py
+++ day03Homework_1.py
@@ -4,55 +4,55 @@
 while True:
     Options = input("""1) Fahrenheit to Celsius, 2) Celsius to Fahrenheit, 
     3) Prime Number scaner, 4) Scaner between two number 5) Quit : """)
-    sel = '12345q'
-    if Options in sel:
-        if Options == '1':
-            Fahrenheit = float(input("Enter the Fahrenheit. : "))
-            print(f"{Fahrenheit}F is {(Fahrenheit - 32) * 5 / 9:.4f}C")
 
-        elif Options == '2':
-            Celsius = float(input("Enter the Celsius. : "))
-            print(f"{Celsius}C is {(Celsius * 9.0 / 5.0) + 32:.4f}F")
+    if Options == '1':
+        Fahrenheit = float(input("Enter the Fahrenheit. : "))
+        print(f"{Fahrenheit}F is {(Fahrenheit - 32) * 5 / 9:.4f}C")
 
-        elif Options == '3':
-            num = int(input("Enter the number. : "))
-            is_prime = True
-            if num < 2:
-                print(f"{num} is NOT prime number.")
+    elif Options == '2':
+        Celsius = float(input("Enter the Celsius. : "))
+        print(f"{Celsius}C is {(Celsius * 9.0 / 5.0) + 32:.4f}F")
+
+    elif Options == '3':
+        num = int(input("Enter the number. : "))
+        is_prime = True
+        if num < 2:
+            print(f"{num} is NOT prime number.")
+        else:
+            for i in range(2,num):
+                if num % i == 0:
+                    is_prime = False
+                    break
+                    i += 1
+            if is_prime:
+                print(f"{num} is prime number.")
             else:
-                for i in range(2,num):
-                    if num % i == 0:
+                print(f"{num} is NOT prime number.")
+
+    elif Options == '4':
+        numbers = input("Enter the number1 & number2. : ").split()
+        n1 = int(numbers[0])
+        n2 = int(numbers[1])
+        if n1 > n2:
+            n1, n2 = n2, n1
+
+        for number in range(n1, n2 + 1):
+            is_prime = True  # is_prime 을 반복 마다 True 로 주어야함.
+
+            if number < 2:
+                pass
+            else:
+                for i in range(2, number):
+                    if number % i == 0:
                         is_prime = False
                         break
-                        i += 1
                 if is_prime:
-                    print(f"{num} is prime number.")
-                else:
-                    print(f"{num} is NOT prime number.")
+                    print(number, end=' ') #줄 바꿈 대신에 white space
+        print('') # 줄 바꿈 위해 들어감.
 
-        elif Options == '4':
-            numbers = input("Enter the number1 & number2. : ").split()
-            n1 = int(numbers[0])
-            n2 = int(numbers[1])
-            if n1 > n2:
-                n1, n2 = n2, n1
-
-            for number in range(n1, n2 + 1):
-                is_prime = True  # is_prime 을 반복 마다 True 로 주어야함.
-
-                if number < 2:
-                    pass
-                else:
-                    for i in range(2, number):
-                        if number % i == 0:
-                            is_prime = False
-                            break
-                    if is_prime:
-                        print(number, end=' ') #줄 바꿈 대신에 white space
-            print('') # 줄 바꿈 위해 들어감.
-
-        elif Options == '5' or 'q':
-            print("Goodbye Have a nice day ^~^")
-            break
+    elif Options == '5' or 'q':
+        print("Goodbye Have a nice day ^~^")
+        break
     else:
         print("Invalid options! Please Enter the number between '1~5' or key 'q' ")
+# 문제 1 예외처리, 소수옵션 3,4번의 코드 중복 -> 함수로 해결
---
+++ day04.py
@@ -0,0 +1,22 @@
+numbers = input("Enter the number1, number2.").split()
+n1 =int(numbers[0])
+n2 =int(numbers[1])
+if n1 > n2:
+    n1, n2 = n2, n1
+
+for number in range(n1, n2+1):
+    is_prime = True # is_prime 을 반복 마다 True 로 주어야함.
+
+    if number < 2:
+        pass
+    else:
+        i = 2
+        while i*i < number: # i^2을 해줌으로서 소수에 대한 성능 이슈 해결
+            if number % i ==0:
+                is_prime = False
+                break
+            i += 1
+            print(i,end=' ')
+
+        if is_prime: print(number, end=' ')
+# number 같은 것은 for 문 안에서만 작동하기 때문에 선언을 따로 하지 않아도 됨.
