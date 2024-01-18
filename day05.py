1f6c4750bf375c2fb71fa3341ef71b1abb423253
light <agy91521947@gmail.com>
2024-01-18 18:59:19 +0900

    v4.5 homework9.2

--- day05.py
+++ day05.py
@@ -1,29 +1,47 @@
-# decorator
-def description(f):  # closure
-    def inner(*args):
-        print(f.__name__)
-        print(f.__doc__)
-        r = f(*args)
-        return r
-
-#
-def squares(n):
-    """
-    제곱 함수
-    """
-    return n * n
-
-@description
-def power(b, e):
-    """
-    거듭제곱 함수
-    """
-    result = 1
-    for _ in range(e):
-        result = result * b
-    return result
+# '[Harry','Ron','Hermione']를 리스트 형태로 반환
+def good(a):
+     print(list(a))
 
 
-f1 = description(squares)
-print(f1(9))
-print(power(2, 10))
+name = 'Harry','Ron','Hermione'
+z = 'Harry'
+x = 'Ron'
+c = 'Hermione'
+q = z,x,c
+good(name)
+goodori(q)
+
+
+# generator
+def my_range(first=0, last=10, step=1):
+    number = first
+    while number < last:
+        yield number
+        number += step
+ran = my_range(10)
+
+for x in ran:
+     if x%2==1:
+          if x.count()==3:
+               print(x)
+
+
+# description
+
+
+
+
+
+
+#in my brain
+def get_odds(n):
+     result = []
+     for x in n :
+          if x%2 ==1:
+               result.append(x)
+     print(result[2])
+
+
+r = range(10)
+get_odds(r)
+
