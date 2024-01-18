1f5869da1b5c14110b43f0714ae7200d4c7ced48
light <agy91521947@gmail.com>
2024-01-18 09:25:15 +0900

    3.6 assignment of day04

--- day04.py
+++ day04.py
@@ -1,28 +1,42 @@
+# practice1
 # e2f = dick(dog='chien', cat='chat', walrus='morse')
-# mans = dict(marx='russian', humist='bin', kant='g')
-#
+
+
 # # list(animal)
 # print(e2f["walrus"])
 # print(e2f.get('walrus'))
 # print(list(e2f.keys()))
-# e2f.update(mans)
+
 # print(e2f)
 
 # a = list(e2f.items())
-#
+# practice3
 # f2e = {a:b for b,a in e2f.items()}# comprehension
 # print (f2e)
 # print(f2e['chien'])
 #
 # #
 # print(e2f.keys())
-life = {'animals':{'cats':'Henri','octopi':'Grumpy','emus':'Lucy'},
- 'plants':{'':''},
- 'other':{'':''}}
-print(life.keys())
-print(life['animals'].keys())
-print(life['animals']['cats'])
-
-squares = {i:i*i for i in range(10)}
+# life = {'animals':{'cats':'Henri','octopi':'Grumpy','emus':'Lucy'},
+#  'plants':{'':''},
+#  'other':{'':''}}
+# print(life.keys())
+# print(life['animals'].keys())
+# print(life['animals']['cats'])
+#
+squares = {i:i*i for i in range(10)} # 축약 표현식
 print(squares)
+
+# comprehension
+# univ = 'Inha university'
+# # counts_alphabet = {L:univ.count(L) for L in univ}
+# # print(counts_alphabet)
+#
+# # old
+# counts_alphabet = dict()
+# for l in univ:
+#   counts_alphabet[l] = univ.count(l)
+# print(counts_alphabet)
+#
+
 
