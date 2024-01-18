a6b0e982edf58186fe3e642a1dbea6a163e11e21
light <agy91521947@gmail.com>
2024-01-18 14:12:34 +0900

    v4.0 closure

--- day05.py
+++ day05.py
@@ -1,18 +1,14 @@
-def squares(*n)->list:
-    """
-
-    :param n:
-    :return:
-    """
-    return [i**2 for i in n]
-
-
-    # return n**2
-
-def rum_function(f, *number)-> list:
-    return f(*number)
-
-print(squares(7,5,2))
-print(rum_function(squares,9,10))
+# def out_func(nout):
+#     def inner_func(nin):
+#         return nin*nin
 #
-#
+#     return out_func(nout)
+# print(out_func(5))
+
+def out_func(nout):
+    def inner_func():
+        return nout*nout
+
+    return inner_func()
+print(out_func(5))
+#크로저는 바깥 함수의 매개변수와 자신의주소 또한 가지고 있다.?
