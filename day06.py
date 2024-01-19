4201a2320566947ffa4ba211960609878527b2d1
light <agy91521947@gmail.com>
2024-01-19 11:13:10 +0900

    v5.1 raise exception

--- day06.py
+++ day06.py
@@ -1,5 +1,9 @@
 import random
 
+class OopsException(Exception):
+    pass
+
+
 # numbers = list()
 # for i in range(5)
 #     numbers.append(random.append(1,100))
@@ -9,12 +13,16 @@ print(numbers)
 try:
     pick = int(input(f"Input the number(0~{len(numbers)-1}) : "))
     print(numbers[pick])
+    print(5/2)
+    raise OopsException("Oops!") # 클래스를 활용한 강제 예외 발생
 
 except IndexError as err: # as + 변수 이름 하면 시스템에서 만든 문구를 출력
     print(f"out of range: Wrong index number {err}")
 except ValueError:
     print("Input only number")
+
 except Exception: #이러한 것은 보험용이기에 맨 밑에 넣어야함
     print("error occurs")
-
+else :
+    print("terminate program")
 # except 옆에 밑에 창에 뜨는 시스템 에러 이름을 적어주면 됨.
