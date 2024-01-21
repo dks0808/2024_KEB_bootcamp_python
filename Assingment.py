c479868ae6d50f61f5a737a539f6cea0b5f5bbf9
light <agy91521947@gmail.com>
2024-01-21 12:32:45 +0900

    v5.9 dictionary

--- Assingment.py
+++ Assingment.py
@@ -1,26 +1,25 @@
 # Pokemon game assignment
 import random
 
+# attack class control this for dick
 class W_attackMixin:
     def water(self):
         return f"{self.__name}이(가) 하늘을 훨훨 날아갑니다~"
-
 class F_attackMixin:
     def flame(self):
         return f"{self.__name}이(가) 수영을 합니다."
-
 class E_attackMixin:
     def electric(self):
         return f"{self.__name}이(가) 수영을 합니다."
-
 class Plant_attackMixin: # never function in Pikachu
     def plant(self):
         return f"{self.__name}이(가) 수영을 합니다."
-
 class FlyMixin:
     def Fly(self):
         return f'fly'
 
+
+# basic evolution attribute class with Top Class
 class Pokemon:
     def __init__(self, name):
         self.__name = name
@@ -49,9 +48,8 @@ class Pokemon:
         return ev
 
 
-    #name = property(get_name, set_name)
 
-
+# basic evolution level class
 class Charmander(Pokemon,F_attackMixin):
     def mana_point(self):
         mp = 100
@@ -67,7 +65,6 @@ class Charmander(Pokemon,F_attackMixin):
     def type(self):
         t = 'Fire'
         return t
-
 class Bulbasaur(Pokemon,Plant_attackMixin):
     def mana_point(self):
         mp = 100
@@ -82,7 +79,6 @@ class Bulbasaur(Pokemon,Plant_attackMixin):
     def type(self):
         t = 'Plant'
         return t
-
 class Gyarados(Pokemon,W_attackMixin):
     def mana_point(self):
         mp = 100
@@ -97,7 +93,6 @@ class Gyarados(Pokemon,W_attackMixin):
     def type(self):
         t = 'Fly','Water'
         return t
-
 class Pikachu(Pokemon,E_attackMixin):
     def mana_point(self):
         mp = 100
@@ -112,7 +107,6 @@ class Pikachu(Pokemon,E_attackMixin):
     def type(self):
         t = 'Electric'
         return t
-
 class Squirtle(Pokemon,W_attackMixin):
     def mana_point(self):
         mp = 100
@@ -128,8 +122,8 @@ class Squirtle(Pokemon,W_attackMixin):
         t = 'Water'
         return t
 
-
-class Charmander_2(Pokemon, F_attackMixin,Charmander):
+# advance evolutions class
+class Charmander_2(Charmander, F_attackMixin):
     def mana_point(self):
         mp = 250
         return mp
@@ -137,9 +131,7 @@ class Charmander_2(Pokemon, F_attackMixin,Charmander):
     def heart_point(self):
         hp = 150
         return hp
-
-
-class Bulbasaur_2(Pokemon, Plant_attackMixin,Bulbasaur):
+class Bulbasaur_2(Bulbasaur, Plant_attackMixin):
     def mana_point(self):
         mp = 100
         return mp
@@ -147,8 +139,7 @@ class Bulbasaur_2(Pokemon, Plant_attackMixin,Bulbasaur):
     def heart_point(self):
         hp = 100
         return hp
-
-class Gyarados_2(Pokemon, W_attackMixin, Gyarados):
+class Gyarados_2(Gyarados, W_attackMixin):
     def mana_point(self):
         mp = 120
         return mp
@@ -156,9 +147,7 @@ class Gyarados_2(Pokemon, W_attackMixin, Gyarados):
     def heart_point(self):
         hp = 300
         return hp
-
-
-class Pikachu_2(Pokemon, E_attackMixin, Pikachu):
+class Pikachu_2(Pikachu, E_attackMixin):
     def mana_point(self):
         mp = 200
         return mp
@@ -166,9 +155,7 @@ class Pikachu_2(Pokemon, E_attackMixin, Pikachu):
     def heart_point(self):
         hp = 150
         return hp
-
-
-class Squirtle_2(Pokemon, W_attackMixin, Squirtle):
+class Squirtle_2(Squirtle, W_attackMixin):
     def mana_point(self):
         mp = 150
         return mp
@@ -178,38 +165,79 @@ class Squirtle_2(Pokemon, W_attackMixin, Squirtle):
         return hp
 
 
-# naming
+# basic evolution level class naming partition
 p1 = Pikachu("Pikachu")
 c1 = Charmander("Charmander")
 b1 = Bulbasaur("Bulbasaur")
 g1 = Gyarados("Gyarados")
 s1 = Squirtle("Squirtle")
 
+# advance evolutions class naming partition
 p2 = Pikachu_2("Pikachu2")
 c2 = Charmander_2("Charmander2")
 b2 = Bulbasaur_2("Bulbasaur2")
 g2 = Gyarados_2("Gyarados2")
 s2 = Squirtle_2("Squirtle2")
 
-# the attribute
-p1a = [p1.name, p1.heart_point(), p1.mana_point(), p1.type() ]
-c1a = [c1.name, c1.heart_point(), c1.mana_point(), c1.type() ]
-b1a = [b1.name, b1.heart_point(), b1.mana_point(), b1.type() ]
-g1a = [g1.name, g1.heart_point(), g1.mana_point(), g1.type() ]
-s1a = [s1.name, s1.heart_point(), s1.mana_point(), s1.type() ]
+def bea(numbers): # basic evolution attributes dictionary
+    p1a = {"Name": p1.name, 'Hp': p1.heart_point(), 'Mp' : p1.mana_point(), 'Type': p1.type()}
+    c1a = {"Name": c1.name, 'Hp': c1.heart_point(), 'Mp' : c1.mana_point(), 'Type': c1.type()}
+    b1a = {"Name": b1.name, 'Hp': b1.heart_point(), 'Mp' : b1.mana_point(), 'Type': b1.type()}
+    g1a = {"Name": g1.name, 'Hp': g1.heart_point(), 'Mp' : g1.mana_point(), 'Type': g1.type()}
+    s1a = {"Name": s1.name, 'Hp': s1.heart_point(), 'Mp' : s1.mana_point(), 'Type': s1.type()}
+    if numbers == '1':
+        return p1a
+    elif numbers == '2':
+        return c1a
+    elif numbers == '3':
+        return b1a
+    elif numbers == '4':
+        return g1a
+    elif numbers == '5':
+        return s1a
 
-p2a = [p2.name, p1.heart_point(), p1.mana_point(), p1.type() ]
-c2a = [c2.name, c2.heart_point(), c2.mana_point(), c2.type() ]
-b2a = [b2.name, b2.heart_point(), b2.mana_point(), b2.type() ]
-g2a = [g2.name, g2.heart_point(), g2.mana_point(), g2.type() ]
-s2a = [s2.name, s2.heart_point(), s2.mana_point(), s2.type() ]
+
+def aea(numbers): # advance evolution attributes dictionary
+    p2a = {"Name": p2.name, 'Hp': p2.heart_point(), 'Mp' : p2.mana_point(), 'Type': p2.type()}
+    c2a = {"Name": c2.name, 'Hp': c2.heart_point(), 'Mp' : c2.mana_point(), 'Type': c2.type()}
+    b2a = {"Name": b2.name, 'Hp': b2.heart_point(), 'Mp' : b2.mana_point(), 'Type': b2.type()}
+    g2a = {"Name": g2.name, 'Hp': g2.heart_point(), 'Mp' : g2.mana_point(), 'Type': g2.type()}
+    s2a = {"Name": s2.name, 'Hp': s2.heart_point(), 'Mp' : s2.mana_point(), 'Type': s2.type()}
+    if numbers == '1':
+        return p2a
+    elif numbers == '2':
+        return c2a
+    elif numbers == '3':
+        return b2a
+    elif numbers == '4':
+        return g2a
+    elif numbers == '5':
+        return s2a
+
+
+# basic evolution attributes ver list
+# p1a = [p1.name, p1.heart_point(), p1.mana_point(), p1.type()]
+# c1a = [c1.name, c1.heart_point(), c1.mana_point(), c1.type()]
+# b1a = [b1.name, b1.heart_point(), b1.mana_point(), b1.type()]
+# g1a = [g1.name, g1.heart_point(), g1.mana_point(), g1.type()]
+# s1a = [s1.name, s1.heart_point(), s1.mana_point(), s1.type()]
+
+# # advance evolution attributes ver list
+# p2a = [p2.name, p2.heart_point(), p2.mana_point(), p2.type()]
+# c2a = [c2.name, c2.heart_point(), c2.mana_point(), c2.type()]
+# b2a = [b2.name, b2.heart_point(), b2.mana_point(), b2.type()]
+# g2a = [g2.name, g2.heart_point(), g2.mana_point(), g2.type()]
+# s2a = [s2.name, s2.heart_point(), s2.mana_point(), s2.type()]
+
+
 
 menu = input(f"Choose your own pokemon. \n 1) Pikachu, 2) Charmander, 3) Bulbasaur, 4)Gyarados, 5)Squirtle, 6) Quit 'press 6 or q' : ")
 options = '12345q'
 while menu in options:
-    if menu ==1:
+    if menu =='1':
         print('You got the pikachu!')
 
         print(f'Pikachu is {p1a}')
+    else:
+        print("Quit this program.")
         break
-
